7100a719adbd60a4bc6b7a751e804bdc5e45f468
Anderson Silva <nutriandersonsilva@gmail.com>
2023-03-17 18:51:28 -0300

    Requisito 3

--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Game from '../pages/Game';
 import Login from '../pages/Login';
+import Config from '../pages/Config';
 
 export default class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route path="/game" component={ Game } />
+        <Route exact path="/config" component={ Config } />
         <Route exact path="/" component={ Login } />
       </Switch>
     );
