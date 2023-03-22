acc24f5646e0773e0cb1479b792503c381e12a20
guhpissai <gustavopissai3d@gmail.com>
2023-03-22 17:39:42 -0300

    Implementando mensagem, placar e acertos na pagina de feedback - Requisito 13 e 14

--- src/components/TriviaQuestion.js
+++ src/components/TriviaQuestion.js
@@ -7,6 +7,7 @@ import {
   disabledButton,
   indexChange,
   playerScore,
+  hits,
 } from '../redux/actions';
 
 class TriviaQuestion extends Component {
@@ -19,12 +20,13 @@ class TriviaQuestion extends Component {
 
   scorePlayer = (answer, target) => {
     const { seconds } = this.state;
-    const { eachQuestion } = this.props;
+    const { eachQuestion, dispatch } = this.props;
     const correctAnswer = target;
     const { difficulty } = eachQuestion;
     const ten = 10;
     const three = 3;
     if (correctAnswer === answer) {
+      dispatch(hits());
       if (difficulty === 'easy') {
         return ten + seconds;
       }
@@ -111,9 +113,9 @@ class TriviaQuestion extends Component {
   render() {
     const correct = 'correct-answer';
     const { eachQuestion, isDisabled } = this.props;
-    // if (!eachQuestion) {
-    //   return (<p>Error</p>);
-    // }
+    if (!eachQuestion) {
+      return (<p>Error</p>);
+    }
     const { question, category } = eachQuestion;
     const correctAnswer = eachQuestion.correct_answer;
     const shuffledAnswers = this.shufflerCondition();
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { assertions, score } = this.props;
+    const three = 3;
     return (
       <div>
         <Header />
+        <p data-testid="feedback-text">
+          {
+            assertions < three ? 'Could be better...' : 'Well Done!'
+          }
+        </p>
+        <div className="scoreboard">
+          <p data-testid="feedback-total-score">{ score }</p>
+          <p data-testid="feedback-total-question">{ assertions }</p>
+        </div>
       </div>
     );
   }
 }
 
-export default connect()(Feedback);
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+  score: state.player.score,
+});
+
+export default connect(mapStateToProps)(Feedback);
